493bf4e922e3918817fb5d10b69cff517439d697
armab2005 <69154178+armab2005@users.noreply.github.com>
2020-09-05 22:26:13 +0530

    Create Amstats.py

---
+++ Amstats.py
@@ -0,0 +1,12 @@
+#created by AM Technology
+
+import numpy as np
+am=[382,367,5528,5527,5527,5527,5527,5318,5529,6820,4428,99257,5527,5528,5528]
+an=[272,26,517,2517,6528,5427,4318,5267,5527,5526,5161,5426,6262,5627,5427]
+ar=an+am
+arr=np.array(ar)
+print(arr.shape)
+print ('=========AM STASTICAL MACHINE========')
+arr.reshape((15,2))
+print(arr.reshape((15,2)))
+print('=========@author:->Arnab Maiti=========')
